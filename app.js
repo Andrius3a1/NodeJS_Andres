afd7b748e5b077d296be1975bacc349bf002f3a5
Andrius <adesseno.arq@gmail.com>
2022-08-13 19:17:30 -0300

    mandando fruta con heroku que me complica la vida

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + "/views"));
+
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        title1: "t1",
+    })
+});
+
+app.get("/services", (req, res) => {
+    res.render("services", {
+        titleServices1: "Services1"
+    })
+});
+
+app.get("/products", (req, res) => {
+    res.render("products", {
+        titleProduct1: "producto 1"
+    })
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact", {
+        titleContact1: "contact 1",
+    })
+})
+
+app.use((req, res, next) => {
+    res.status(404).render("404", {
+        title404: "This is 404 page",
+        dinamicTitle: "Dinamic title from app.js"
+    });
+})
+
+app.listen(port, () => {
+    console.log(`{Server listen on http://localhost:${port}`);
+});
---
+++ pruebas.js
@@ -0,0 +1,10 @@
+// const fechaActual = new Date();
+// const fecha = {
+//     year: fechaActual.getFullYear(),
+//     month: fechaActual.getMonth(),
+//     day: fechaActual.getDay(),
+//     hour: fechaActual.getHours(),
+// }
+// const colores = ["azul", "rojo", "amarillo"]
+
+// module.exports = { fecha, fechaActual, colores }
